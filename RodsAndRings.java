50fbad08e3953c0db6e0a57cd0960b5e4803a0d9
Gavldac <75696834+Gavldac@users.noreply.github.com>
2024-09-30 17:34:07 -0600

    Update RodsAndRings.java

--- RodsAndRings.java
+++ RodsAndRings.java
@@ -1,54 +1,43 @@
 
-import java.util.stream.IntStream;
-
-
-
-
 /**
- * Class to determine if a rod in a ring toss game has 3 different 
- * colors on it.
- * If a rod has all 3 colors on it then it is counted once.
- * Some rods may have no rings, some may have several.
+ * This class determines how many rods in a ring toss game have all three colors of rings: red, green, and blue.
+ * A rod is counted once if it has at least one ring of each color. 
+ * Note that some rods may have no rings, while others may have multiple rings of varying colors.
  * 
  * @author Edwin Casady and Minh Vu
  */
 public class RodsAndRings {
 
     /**
-     * Counts the numder of rods that have all three color possibilities. 
-     * (Think of this like a ring toss game where you get one point if a rod has all 3 colors on it)
+     * Counts the number of rods that have at least one ring of each color (R - Red, G - Green, B - Blue).
      * 
-     * The rings and the rod that they are on is determinied my a String argument such as: "B0B6G0R6R0R6G9"
-     * The rings are simply noted by their color R or G or B for red, green, blue.
-     * Rods are listed by the numbers 0 through 9. 
-     * Some rods may have more than one of any possible color.
+     * In the context of a ring toss game, you earn a point for each rod that displays all three colors.
      * 
-     * The length of the String argument will vary.
-     * The number of rods is always 10 and indexed as 0 through 9.
+     * The input is a formatted string representing rings and their corresponding rod positions.
+     * For example, "B0B6G0R6R0R6G9" indicates:
+     * - 'B0' means a blue ring on rod 0
+     * - 'B6' means a blue ring on rod 6
+     * - 'G0' means a green ring on rod 0
+     * - and so forth.
      * 
-     * @param String rings - formated String to indicate ring color and rod number pairs
-     * @return int rods - the count of all rods that have all 3 color options of rings
+     * Each rod is indexed from 0 to 9, and the total number of rods is fixed at 10.
+     * The length of the input string can vary, but it will always be an even number, as it consists of color-position pairs.
+     * 
+     * @param rings A string formatted as color-position pairs (e.g., "R3G2B1").
+     * @return int The count of rods that have at least one ring of each color (R, G, B).
      */
     private static int countRodsWith3Colors(String rings) {
         /*
-         * There are 0 to n rings. 
-         * Each ring is either red, green, or blue. (R,G,B)
-         * The rings are distributed across ten rods indexed from 0 to 9.
-         * 
-         * You are given a string rings of length 2n that describes the n rings that are placed onto the rods. 
-         * Every two characters in rings forms a color-position pair that is used to describe each ring where:
-         *      The first character of the ith pair denotes the ith ring's color ('R', 'G', 'B').
-         *      The second character of the ith pair denotes the rod that the ith ring is placed on ('0' to '9').
-         * 
-         *      For example, "R3G2B1" describes n == 3 rings: 
-         *          a red ring placed onto the rod labeled 3, 
-         *          a green ring placed onto the rod labeled 2, 
-         *          and a blue ring placed onto the rod labeled 1.
-         * 
-         * Return the number of rods that have all three colors of rings on them.
+         * Each ring is represented by a color ('R', 'G', 'B') followed by the rod number ('0' to '9').
+         * For example, "R3G2B1" indicates:
+         * - A red ring on rod 3
+         * - A green ring on rod 2
+         * - A blue ring on rod 1
+         *
+         * The method will return the total number of rods that contain rings of all three colors.
          */
 
-        return 0; //TODO 
+       return 0; //TODO 
     }
 
 //-----------------TEST CLIENT------------------------//
@@ -60,27 +49,27 @@ public class RodsAndRings {
         String test4 = "B7B1G6G0R9B3R1R1R7R1R1B1G7R8B2B0R0G9B9";
         String test5 = "";
 
-        //test1
+        //Test case 1
         System.out.println("Expected count = 1");
         System.out.println("Tested results = " + countRodsWith3Colors(test1));
         System.out.println();
 
-        //test2
+        //Test case 2
         System.out.println("Expected count = 1");
         System.out.println("Tested results = " + countRodsWith3Colors(test2));
         System.out.println();
 
-        //test3
+        //Test case 3
         System.out.println("Expected count = 0");
         System.out.println("Tested results = " + countRodsWith3Colors(test3));
         System.out.println();
 
-        //test4
+        //Test case 4
         System.out.println("Expected count = 3");
         System.out.println("Tested results = " + countRodsWith3Colors(test4));
         System.out.println();
 
-        //test
+        //Test case 5
         System.out.println("Expected count = 0");
         System.out.println("Tested results = " + countRodsWith3Colors(test5));
         System.out.println();
